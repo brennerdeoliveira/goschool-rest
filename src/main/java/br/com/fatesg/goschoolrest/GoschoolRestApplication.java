a4f272088c4d95f541ec57394a86edd6f4ee9757
brennerdeoliveira <brenner.deoliveira@hotmail.com>
2018-10-28 14:58:42 -0300

    Creating project with Spring Boot

---
+++ src/main/java/br/com/fatesg/goschoolrest/GoschoolRestApplication.java
@@ -0,0 +1,12 @@
+package br.com.fatesg.goschoolrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoschoolRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoschoolRestApplication.class, args);
+	}
+}
